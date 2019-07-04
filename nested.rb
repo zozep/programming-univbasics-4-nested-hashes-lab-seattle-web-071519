809a02013471aef2ce14f5279bd5f30b18b5028f
Joseph P <jpark041@gmail.com>
2019-07-04 04:15:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,7 +100,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :alans_new_info = "GUI" => "GUI",
+          :alans_new_info => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
