be7e871a859f683467a7ed6b66de84f8c91e3dce
Joseph P <jpark041@gmail.com>
2019-07-04 04:12:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C"]
+          :languages => "C"
         }
      }
      
