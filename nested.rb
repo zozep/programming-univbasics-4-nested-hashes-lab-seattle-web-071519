2b433c037b4ce355813b6452e41fc1af5ed2a8e1
Joseph P <jpark041@gmail.com>
2019-07-04 04:14:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -100,7 +100,7 @@ def changing_alan
           :languages => ["COBOL", "FORTRAN"]
         },
         :alan_kay => {
-          :known_for => "Object Orientation",
+          :known_for => "GUI",
           :languages => ["Smalltalk", "LISP"]
         },
         :dennis_ritchie => {
